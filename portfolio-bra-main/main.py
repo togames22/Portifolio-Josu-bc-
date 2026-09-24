8e9ccf4d2ee072a54d5724947e6d44ad7ff3675f
togames22 <josuebarradascamargo@gmail.com>
2026-09-24 16:36:14 -0300

    Add files via upload

---
+++ portfolio-bra-main/main.py
@@ -0,0 +1,36 @@
+# Importar
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# Conteúdo da página
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Habilidades Dinâmicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button.db')
+
+    email = request.form.get('email')
+    text = request.form.get('text')
+    return render_template(
+        'index.html',
+        button_python=button_python,
+        button_discord=button_discord,
+        button_html=button_html,
+        button_db=button_db,
+        email=email,
+        text=text
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
